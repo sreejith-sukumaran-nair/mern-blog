2ad0512e9015c25a71a1f050ba2b359ec58dd1e9
Sreejith Sukumaran Nair <sreejith1931991@gmail.com>
2024-09-12 07:37:20 +0530

    implemented dark mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import ThemeProvider from "./components/themeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import  userReducer from './user/userSlice.js'
+import themeSlice from './theme/themeSlice.js';
 
 export const store = configureStore({
   reducer: {
     user : userReducer ,
+    theme : themeSlice ,
   },
 })
